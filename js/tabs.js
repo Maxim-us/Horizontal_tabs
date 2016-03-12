efe88fa3b9ce5287d253634ce28e1bcad1cb6a38
Maksym Marko <maximus21@mail.ru>
2016-03-12 22:56:07 +0200

    Tabs smoothly open and close

--- js/tabs.js
+++ js/tabs.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){	
 	
+	$('.mx-tab > span').click(function(){		
+		
+		if(!$(this).parent().hasClass('mx-active')){
+			$('.mx-tab').removeClass('mx-active');
+			$('.mx-tab').css('height', '56px');
+			$(this).parent().addClass('mx-active');
+			$(this).parent().animate({'height': '156px'}, 500);
+		}
+		else if($(this).parent().hasClass('mx-active')){
+			$(this).parent().removeClass('mx-active');
+			$(this).parent().animate({'height': '56px'}, 500);
+		}
+	});
+	
 })
