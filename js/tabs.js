5d919b726a3cb9d2f03b08e1c8600bd2405ef9d3
Maksym Marko <maximus21@mail.ru>
2016-03-13 08:01:13 +0200

    Tabs are working with the text in the inside

--- js/tabs.js
+++ js/tabs.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){	
 	
-	$('.mx-tab > span').click(function(){		
-		
+	$('.mx-tab > span').click(function(){
+				
 		if(!$(this).parent().hasClass('mx-active')){
 			$('.mx-tab').removeClass('mx-active');
 			$('.mx-tab').css('height', '56px');
 			$(this).parent().addClass('mx-active');
-			$(this).parent().animate({'height': '156px'}, 500);
+			$(this).parent().animate({'height': '256px'}, 500);
 		}
 		else if($(this).parent().hasClass('mx-active')){
 			$(this).parent().removeClass('mx-active');
